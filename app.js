6f6827cee186aff25235652c687edc270aae4f5b
Aimé Sessou <aimesessou@Aimes-iMac.attlocal.net>
2024-02-01 22:38:50 -0500

    Added user login option

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// import dependencies
 require('dotenv').config()
 const express = require('express');
 const port = process.env.PORT || 3000;
@@ -16,53 +17,60 @@ const server = createServer(app);
 const io = require('socket.io')(server)
 const mongoose = require('mongoose');
 
+// initialise dependencies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'my-secret-key' , resave: false, saveUninitialized: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/public', express.static(path.join(__dirname, 'public')))
+//implement hashing for sensitive informations
 
 const secret = process.env.SECRET;
-const saltRounds = 10; 
 let correctPasswordHash;
-
-
-
-bcrypt.hash(secret, saltRounds, (err, hash) => {
-  if (err) {
-   
+bcrypt.hash(secret, 10, (err, hash) => {
+  if (err) { 
   } else {
      correctPasswordHash = hash;
-
   }
 });
 
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ secret: 'my-secret-key' , resave: false, saveUninitialized: true }));
+
 
 
 
 // Local MongoDB
-// mongoose.connect('mongodb://127.0.0.1:27017/posterDB');
+const posterDB = mongoose.createConnection('mongodb://127.0.0.1:27017/posterDB');
+
 
 
 
 // // // Remote MongoDB
-const uri = process.env.MONGODB_URI;
+// const uri = process.env.MONGODB_URI;
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('MongoDB Connected...'))
+//   .catch(err => console.log(err));
 
-
+//Create a Poster schema
 const posterSchema = {
+    ID: String,
     name: String,
     date: Date
 };
 
-const Poster =mongoose.model("Poster", posterSchema)
+const Poster = posterDB.model("Poster", posterSchema)
 
+//Create a User Schema
+const userDB = mongoose.createConnection('mongodb://127.0.0.1:27017/userDB');
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use('/public', express.static(path.join(__dirname, 'public')))
+const userSchema = {
+  email: String,
+  password: String
+}
+
+const User = userDB.model("User", userSchema)
 
 
 
@@ -105,27 +113,32 @@ app.get("/board", async (req, res) => {
 });
 
 app.get('/upload', (req, res)=>{
+    console.log(req.session)
         res.render("upload")
     })
     
 app.get("/success")
 
-app.post('/upload', posterUpload().array('images', 1), (req, res, next) => {
+app.post('/upload', posterUpload().array('images', 1), async (req, res, next) => {
   if (!req.files || req.files.length === 0 ) {
       return res.status(400).send('No files uploaded.');
   } else if( req.files.length > 1){
     return res.status(400).send('Please pick one poster.')
   }
-    
+    const posterEmail= await User.find({email:req.body.email})
+    const posterID = posterEmail[0]._id
+    console.log(posterID)
     const posterName =  req.files[0].filename;
     const posterDate = req.body.date;
 
     const poster = new Poster({
+        ID: posterID,
         name: posterName,
         date: posterDate
     });
 
     poster.save()
+    
         .then(() => {
             // Emit a Socket.io event when a new poster is uploaded
              io.emit('newPoster', posterName);
@@ -135,13 +148,14 @@ app.post('/upload', posterUpload().array('images', 1), (req, res, next) => {
             console.error('Error saving poster:', err);
             return res.status(500).send('Error saving poster.');
         });
+      console.log(poster)
 });
 
 
 
 
-app.get("/login", (req,res)=>{
-    res.render("login")
+app.get("/admin_login", (req,res)=>{
+    res.render("admin_login")
 })
 
 
@@ -163,7 +177,7 @@ app.get("/admin", async(req, res) => {
     }
     
   } else {
-    res.render("login");
+    res.render("admin_login");
   }
 });
 
@@ -190,7 +204,7 @@ app.post("/admin", async (req, res) => {
 
 
 
-app.post("/login", (req, res) => {
+app.post("/admin_login", (req, res) => {
   const enteredPassword = req.body.password;
   bcrypt.compare(enteredPassword, correctPasswordHash, (err, result) => {
     if (err) {
@@ -209,11 +223,60 @@ app.get("/logout", (req, res) => {
       if (err) {
         console.error("Error destroying session:", err);
       }
-      res.redirect("/login"); // Redirect the user to the login page after logging out
+      res.redirect("/admin_login"); // Redirect the user to the login page after logging out
     });
   });
 
+  app.get("/sign_up", (req, res)=>{
+    res.render("sign_up")
 
+  })
+
+  app.post("/sign_up", (req, res)=>{
+
+    const userEmail = req.body.email
+    const userPassword = req.body.password
+
+    const user = new User({
+      email: userEmail,
+      password: userPassword
+    })
+
+    user.save()
+
+    // console.log(user);
+
+    res.redirect("/board")
+
+  })
+
+
+  app.get("/login", (req, res)=>{
+    res.render("login")
+  })
+  app.post("/login", async (req, res)=>{
+    try{
+
+      
+    const userInfo = await User.find({email: req.body.email})
+    const posterArray = await Poster.find({ID: userInfo[0]._id})
+           
+    
+    if(userInfo[0].email){
+      if( userInfo[0].password == req.body.password){
+      res.render("users",{ posterArray} )
+    }else{
+      res.redirect("/login")
+    }
+  }else{
+    
+  }
+    console.log(userInfo[0].email)
+    }
+    catch{
+      res.redirect("/users")
+    }
+  })
   // Cron-job for autodeletion
 async function removeExpiredEvents() {
     const currentDate = new Date();
